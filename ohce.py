39bfa017a9ed3a7df6fc109e0fc46b6f253a5315
Primo18 <danilobo.elo@gmail.com>
2025-04-06 17:46:41 -0400

    Cycle 3 - Green: Implement time-based greeting functionality

--- ohce.py
+++ ohce.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
+class RealClock:
+    def get_current_hour(self):
+        return datetime.now().hour
+
 class Ohce:
-    def __init__(self, name):
+    def __init__(self, name, clock=None):
         self.name = name
+        self.clock = clock or RealClock()
         
     def reverse_echo(self, text):
         reversed_text = text[::-1]
@@ -13,3 +20,13 @@ class Ohce:
     def _is_palindrome(self, text):
         """Check if a text is a palindrome."""
         return text == text[::-1]
+    
+    def greet(self):
+        hour = self.clock.get_current_hour()
+        
+        if 6 <= hour < 12:
+            return f"¡Buenos días {self.name}!"
+        elif 12 <= hour < 20:
+            return f"¡Buenas tardes {self.name}!"
+        else:  # 20-23 and 0-5
+            return f"¡Buenas noches {self.name}!"
